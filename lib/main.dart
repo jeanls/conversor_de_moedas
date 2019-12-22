95abb58e0bce060e5c7b13bcee7b847f77053fea
jean <jean.leal22@gmail.com>
2019-12-22 20:03:54 -0300

    v1 app conversor de moedas

--- lib/main.dart
+++ lib/main.dart
@@ -30,16 +30,40 @@ class _HomeState extends State<Home> {
   final dolarController = TextEditingController();
   final euroController = TextEditingController();
 
+  void _clearAll(){
+    realController.text = "";
+    euroController.text = "";
+    dolarController.text = "";
+  }
+
   void _realChanged(String text){
-    print(text);
+    if(text.isEmpty){
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
   }
 
   void _dolarChanged(String text){
-    print(text);
+    if(text.isEmpty){
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
   }
 
   void _euroChanged(String text){
-    print(text);
+    if(text.isEmpty){
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
   }
 
   @override
